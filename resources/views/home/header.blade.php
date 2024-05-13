a0c3817863e550f05486f43ed5b9e527fa552c90
warismee <50206850+warismee@users.noreply.github.com>
2024-05-14 00:25:49 +0700

    edit styling and add login link

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -2,7 +2,7 @@
          <div class="header">
             <div class="container">
                <div class="row">
-                  <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                  <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col logo_section">
                      <div class="full">
                         <div class="center-desk">
                            <div class="logo">
@@ -11,7 +11,7 @@
                         </div>
                      </div>
                   </div>
-                  <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                  <div class="col-xl-10 col-lg-10 col-md-10 col-sm-10">
                      <nav class="navigation navbar navbar-expand-md navbar-dark ">
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
@@ -35,6 +35,13 @@
                               </li>
                               <li class="nav-item">
                                  <a class="nav-link" href="contact.html">Contact Us</a>
+                              </li>
+                              
+                              <li class="nav-item">
+                                 <a class="nav-log" href="{{url('login')}}">Login</a>
+                              </li>
+                              <li class="nav-item">
+                                 <a class="nav-log" href="{{url('register')}}">Register</a>
                               </li>
                            </ul>
                         </div>
