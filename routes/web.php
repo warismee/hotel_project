bc02b44aaa27577353617eec60bdbf29b11144be
warismee <50206850+warismee@users.noreply.github.com>
2024-07-15 22:58:46 +0700

    add all midleware

--- routes/web.php
+++ routes/web.php
@@ -23,33 +23,33 @@ Route::get('/',[AdminController::class,'home']);
 
 Route::get('/home',[AdminController::class,'index'])->name('home');
 
-Route::get('/create_room',[AdminController::class,'create_room']);
-Route::post('/add_room',[AdminController::class,'add_room']);
+Route::get('/create_room',[AdminController::class,'create_room'])->middleware(['auth','admin']);
+Route::post('/add_room',[AdminController::class,'add_room'])->middleware(['auth','admin']);
 
-Route::get('/view_room',[AdminController::class,'view_room']);
-Route::get('/deleteroom/{id}',[AdminController::class,'deleteroom']);
+Route::get('/view_room',[AdminController::class,'view_room'])->middleware(['auth','admin']);
+Route::get('/deleteroom/{id}',[AdminController::class,'deleteroom'])->middleware(['auth','admin']);
 
-Route::get('/update_room/{id}',[AdminController::class,'update_room']);
-Route::post('/edit_room/{id}',[AdminController::class,'edit_room']);
+Route::get('/update_room/{id}',[AdminController::class,'update_room'])->middleware(['auth','admin']);
+Route::post('/edit_room/{id}',[AdminController::class,'edit_room'])->middleware(['auth','admin']);
 
 Route::get('/room_details/{id}',[HomeController::class,'room_details']);
 Route::post('/add_booking/{id}',[HomeController::class,'add_booking'])->name('add_booking');
 
 Route::get('/bookings',[AdminController::class,'bookings'])->middleware(['auth','admin']);
-Route::get('/deletebooking/{id}',[AdminController::class,'deletebooking']);
-Route::get('/approve_booking/{id}',[AdminController::class,'approve_booking']);
-Route::get('/reject_booking/{id}',[AdminController::class,'reject_booking']);
+Route::get('/deletebooking/{id}',[AdminController::class,'deletebooking'])->middleware(['auth','admin']);
+Route::get('/approve_booking/{id}',[AdminController::class,'approve_booking'])->middleware(['auth','admin']);
+Route::get('/reject_booking/{id}',[AdminController::class,'reject_booking'])->middleware(['auth','admin']);
 
-Route::get('/view_gallery',[AdminController::class,'view_gallery']);
-Route::post('/upload_gallery',[AdminController::class,'upload_gallery']);
-Route::get('/delete_gallery/{id}',[AdminController::class,'delete_gallery']);
+Route::get('/view_gallery',[AdminController::class,'view_gallery'])->middleware(['auth','admin']);
+Route::post('/upload_gallery',[AdminController::class,'upload_gallery'])->middleware(['auth','admin']);
+Route::get('/delete_gallery/{id}',[AdminController::class,'delete_gallery'])->middleware(['auth','admin']);
 
 Route::post('/contact',[HomeController::class,'contact']);
 
-Route::get('/all_message',[AdminController::class,'all_message']);
-Route::get('/sendmail/{id}',[AdminController::class,'sendmail']);
+Route::get('/all_message',[AdminController::class,'all_message'])->middleware(['auth','admin']);
+Route::get('/sendmail/{id}',[AdminController::class,'sendmail'])->middleware(['auth','admin']);
 
-Route::post('/mail/{id}',[AdminController::class,'mail']);
+Route::post('/mail/{id}',[AdminController::class,'mail'])->middleware(['auth','admin']);
 
 Route::get('/our_rooms',[HomeController::class,'our_rooms']);
 Route::get('/hotel_gallery',[HomeController::class,'hotel_gallery']);
